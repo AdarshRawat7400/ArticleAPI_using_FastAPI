041acf610cf8d315a4214626b4320ed8e8e4b920
Adarsh Rawat <56146385+AdarshRawat7400@users.noreply.github.com>
2023-01-04 19:26:07 +0530

    Update database.py

--- Article_API/articleapi/database.py
+++ Article_API/articleapi/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from decouple import config
 
-SQLALCHEMY_DATABASE_URL =  "postgresql://zouqtyjlpvpemp:4232d62305cbd08974e917c5377f8e89329af5aade28a6f0e9470381a3320a0f@ec2-52-72-125-94.compute-1.amazonaws.com:5432/d10j57p3ql1nkl"
-
+SQLALCHEMY_DATABASE_URL = config("SQLALCHEMY_DATABASE_URI")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
